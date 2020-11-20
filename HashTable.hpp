a2af471e096405bd30174cd0f94260d317e17555
pescoboza <psebastian01@hotmail.com>
2020-11-20 16:28:31 -0700

    Add project files.

---
+++ HashTable.hpp
@@ -0,0 +1,16 @@
+#ifndef HASH_MAP_HPP
+#define HASH_MAP_HPP
+
+#include <array>
+template <typename T, typename K, size_t N>
+class HashMap {
+	
+	std::array<T, N> m_table;
+	std::array<K, N> m_keys;
+
+public:
+
+
+	
+};
+#endif // !HASH_MAP_HPP
