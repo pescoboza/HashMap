c98f35ceb6fe4cbd12267fa232e04284ba12692c
pescoboza <psebastian01@hotmail.com>
2020-11-21 13:53:27 -0700

    :construction: Savin progress before implementing dynamic size hash map.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <string>
 #include "HashMap.hpp"
 
+#include <unordered_map>
+#include <iostream>
+
 int main() {
+
+	std::unordered_map<int, double> map;
+	
+	int numInserts{ 10000000 };
+	int bucketCount{ -1 };
+	for (int i{ 0 }; i < numInserts; ++i) {
+		map.emplace(i, i + i * 0.1);
+		if (map.bucket_count() != bucketCount) {
+			bucketCount = map.bucket_count();
+			std::cout << "i: " << i <<" bc: " <<bucketCount - 1 << std::endl;
+		}
+	}
+
+	std::cout << map.max_bucket_count() << std::endl;
+	std::cout << map.bucket_count() << std::endl;
+	std::cout << map.load_factor() << std::endl;
+
 	HashMap<int, int, 3U> hashMap;
 
 	int k{ 3 };
