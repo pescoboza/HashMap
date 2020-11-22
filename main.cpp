35f42feac885daae558e3aebe79a572f882546e0
pescoboza <psebastian01@hotmail.com>
2020-11-21 18:35:45 -0700

    :boom: Added test structure. Added TODO comments.

--- main.cpp
+++ main.cpp
@@ -48,45 +48,92 @@ using Lookups = std::vector<K>;
 
 
 template <class T, class K>
-void test_template(const MapContent<K ,T>& mapContent, const Lookups<K>& lookups) {
-	size_t mapSize{getSize(mapContent)};
+void chain(const MapContent<K, T>& mapContent, const Lookups<K>& lookups, size_t mapSize, std::ostream& out = std::cout) {
+	HashMapInternalChaining<K, T> map{mapSize};
+
+	out << " ====== INSERTIONS =======\n"
+	for (const auto& entry : mapContent) {
+		auto res{ map.insert(entry.first, entry.second) };
+		out << "try insert: (" << entry.first <<", " << entry.second <<  ") result: " << std::boolalpha << res.first;
+	}
 	
+	out << "\n\n\n";
+	out << " ====== LOOKUPS =======\n"
+
+	for (const auto& key : lookups) {
+		auto res{ map.find(key) };
+		
+		out << "looked up: " << key << " result: ";
+		if (res == nullptr) {
+			out << " NULL"
+		}
+		else {
+			out << res->second;
+		}
+
+	}
+	
+}
+
+template <class T, class K>
+void quadratic(const MapContent<K, T>& mapContent, const Lookups<K>& lookups, size_t mapSize, std::ostream& out  = std::cout) {
+
+	HashMap<int, int> hashMap{PRIMES[4]};
+
+
+	out << " ====== INSERTIONS =======\n"
+		for (const auto& entry : mapContent) {
+			auto res{ map.insert(entry.first, entry.second) };
+			out << "try insert: (" << entry.first << ", " << entry.second << ") result: " << std::boolalpha << res.first;
+		}
+
+	out << "\n\n\n";
+	out << " ====== LOOKUPS =======\n"
+
+		for (const auto& key : lookups) {
+			auto res{ map.find(key) };
+
+			out << "looked up: " << key << " result: ";
+			if (res == nullptr) {
+				out << " NULL"
+			}
+			else {
+				out << res->second;
+			}
+
+		}
+}
+
+template <class T, class K>
+void test_template(const MapContent<K, T>& mapContent, const Lookups<K>& lookups) {
+	size_t mapSize{ getSize(mapContent) };
+
 	chain(mapContent, lookups, mapSize);
 	quadratic(mapContent, lookups, mapSize);
 }
 
 
+// TODO: Iplement the tests
+// TODO: Change linear to quadratic probing
+void test1();
+void test2();
+void test3();
+void test4();
 
-template <class T, class K>
-void chain(const MapContent<K, T>& mapContent, const Lookups<K>& lookups, size_t mapSize, std::ostream& = std::cout) {
-	HashMapInternalChaining<K, T> map{mapSize};
-
-	for (const auto& entry : mapContent) {
-		auto res{ map.insert(entry.first, entry.second) };
-		out << "ty insert: (" << entry.first <<", " << entry.second <<  ") result: " << std::boolalpha << res.first;
-	}
-	
-}
-
-void quadratic() {
-
-	HashMap<int, int> hashMap{PRIMES[4]};
-
-	int k{ 3 };
-
-	hashMap.insert(k, 15);
-	hashMap.insert(k+1, 15);
-
-	auto findRes1{ hashMap.find(k) };
-	auto findRes2{hashMap.find(k+1)};
-
-	hashMap.erase(k);
-
-}
 
 int main() {
+	std::cout << "\n\n=========== TEST 1 ==================" << std::endl;
+	test1();
 	
-	chain();
+	std::cout << "\n\n=========== TEST 2 ==================" << std::endl;
+	test2();
+
+	std::cout << "\n\n=========== TEST 3 ==================" << std::endl;
+	test3();
 	
-	return 0;
+	std::cout << "\n\n=========== TEST 4 ==================" << std::endl;
+	test4();
+
+	std::cout << "Press enter to exit.";
+	std::cin.get();
 }
