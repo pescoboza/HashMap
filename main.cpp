66afc7f3bc39703153ee3bb7c1bf1d0f7ecb9934
pescoboza <psebastian01@hotmail.com>
2020-11-21 18:25:20 -0700

    :construction: Began implementing test templates.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <vector>
+
 
 #include "HashMap.hpp"
 #include "HashMapInternalChaining.hpp"
 
 
-const size_t PRIMES[]{
+const std::vector<size_t> PRIMES{
 	 7U,
 	 63U,
 	 511U,
@@ -25,20 +27,44 @@ const size_t PRIMES[]{
 	 16777215U
 };
 
-void chain() {
-	HashMapInternalChaining<int, int> hashMap{PRIMES[4]};
+template<class Container>
+unsigned getSize(const Container& cont, size_t*, const std::vector<size_t>& table = PRIMES ) {
+	size_t actualSize{ cont.size() };
 
-	int k{ 3 };
+	for (const auto& s : table) {
+		if (s >= actualSize) {
+			return s;
+		}
+	}
 
-	auto a = hashMap.insert(k, 15);
-	auto b = hashMap.insert(k, 16);
-	auto c = hashMap.insert(k + 1, 15);
+	return table[table.size() - 1];
+}
 
-	auto findRes1{ hashMap.find(k) };
-	auto findRes2{ hashMap.find(k + 1) };
+template <class T, class K>
+using MapContent = std::vector<std::pair<K, T>>;
 
-	hashMap.erase(k);
+template <class K>
+using Lookups = std::vector<K>;
 
+
+template <class T, class K>
+void test_template(const MapContent<K ,T>& mapContent, const Lookups<K>& lookups) {
+	size_t mapSize{getSize(mapContent)};
+	
+	chain(mapContent, lookups, mapSize);
+	quadratic(mapContent, lookups, mapSize);
+}
+
+
+
+template <class T, class K>
+void chain(const MapContent<K, T>& mapContent, const Lookups<K>& lookups, size_t mapSize, std::ostream& = std::cout) {
+	HashMapInternalChaining<K, T> map{mapSize};
+
+	for (const auto& entry : mapContent) {
+		auto res{ map.insert(entry.first, entry.second) };
+		out << "ty insert: (" << entry.first <<", " << entry.second <<  ") result: " << std::boolalpha << res.first;
+	}
 	
 }
 
