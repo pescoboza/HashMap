b5f1327a045348fa54da568e9ee39efe9bfc8727
pescoboza <psebastian01@hotmail.com>
2020-11-24 11:46:04 -0700

    :bento: Added IpAddress.hpp.

---
+++ IpAddress.cpp
@@ -0,0 +1,110 @@
+#include "IpAddress.hpp"
+
+#include <sstream>
+
+ip::IpAddress::IpAddress() :
+	m_part1{ 0U },
+	m_part2{ 0U },
+	m_part3{ 0U },
+	m_part4{ 0U },
+	m_port{ 0U }{}
+
+ip::IpAddress::IpAddress(unsigned part1, unsigned part2, unsigned part3, unsigned part4, unsigned port) :
+	m_part1{ part1 }, 
+	m_part2{ part2 }, 
+	m_part3{ part3 }, 
+	m_part4{ part4 }, 
+	m_port{ port }{}
+
+
+ip::IpAddress::IpAddress(const std::string& ipStr) {
+	std::string buff;
+	for (const auto& ch : ipStr) {
+		buff.push_back(std::isdigit(ch) ? ch : ' ');
+	}
+
+	std::istringstream iss{ buff };
+
+	unsigned part1, part2, part3, part4, port;
+	iss >> part1 >> part2 >> part3 >> part4 >> port;
+
+	m_part1 = part1;
+	m_part2 = part2;
+	m_part3 = part3;
+	m_part4 = part4;
+	m_port = port;
+}
+
+
+std::string ip::IpAddress::str() const {
+	std::string buff{ std::to_string(m_part1) };
+	buff.push_back('.');
+	buff.append(std::to_string(m_part2)).push_back('.');
+	buff.append(std::to_string(m_part3)).push_back('.');
+	buff.append(std::to_string(m_part4)).push_back(':');
+	buff.append(std::to_string(m_port));
+	return buff;
+}
+
+
+bool ip::operator==(const ip::IpAddress& l, const ip::IpAddress& r) {
+	return (l.m_part1 == r.m_part1 &&
+		l.m_part2 == r.m_part2 &&
+		l.m_part3 == r.m_part3 &&
+		l.m_part4 == r.m_part4 &&
+		l.m_port == r.m_port);
+}
+
+bool ip::operator>(const ip::IpAddress& l, const ip::IpAddress& r) {
+	if (l.m_part1 > r.m_part1) { return true; }
+	else if (l.m_part1 < r.m_part1) { return false; }
+	else {
+
+		if (l.m_part2 > r.m_part2) { return true; }
+		else if (l.m_part2 < r.m_part2) { return false; }
+		else {
+
+			if (l.m_part3 > r.m_part3) { return true; }
+			else if (l.m_part3 < r.m_part3) { return false; }
+			else {
+
+				if (l.m_part4 > r.m_part4) { return true; }
+				else if (l.m_part4 < r.m_part4) { return false; }
+				else {
+
+					if (l.m_port > r.m_port) { return true; }
+
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
+bool ip::operator<(const ip::IpAddress& l, const ip::IpAddress& r) {
+	if (l.m_part1 < r.m_part1) { return true; }
+	else if (l.m_part1 > r.m_part1) { return false; }
+	else {
+
+		if (l.m_part2 < r.m_part2) { return true; }
+		else if (l.m_part2 > r.m_part2) { return false; }
+		else {
+
+			if (l.m_part3 < r.m_part3) { return true; }
+			else if (l.m_part3 > r.m_part3) { return false; }
+			else {
+
+				if (l.m_part4 < r.m_part4) { return true; }
+				else if (l.m_part4 > r.m_part4) { return false; }
+				else {
+
+					if (l.m_port < r.m_port) { return true; }
+
+				}
+			}
+		}
+	}
+
+	return false;
+}
--- IpAddress.hpp
+++ IpAddress.hpp
@@ -6,33 +6,29 @@
 #include <vector>
 
 
-namespace ip {
-	class IpAddress;
 
-	class IpAddress {
+class IpAddress {
 
 
-	public:
-		unsigned m_part1;
-		unsigned m_part2;
-		unsigned m_part3;
-		unsigned m_part4;
-		unsigned m_port;
+public:
+	unsigned m_part1;
+	unsigned m_part2;
+	unsigned m_part3;
+	unsigned m_part4;
+	unsigned m_port;
 
-		IpAddress();
-		IpAddress(unsigned part1, unsigned part2, unsigned part3, unsigned part4, unsigned port);
-		IpAddress(const std::string& ipStr);
-		std::string str() const;
+	IpAddress();
+	IpAddress(unsigned part1, unsigned part2, unsigned part3, unsigned part4, unsigned port);
+	IpAddress(const std::string& ipStr);
+	std::string str() const;
 
-		friend bool operator==(const ip::IpAddress& rhs, const ip::IpAddress& lhs);
-		friend bool operator>(const ip::IpAddress& rhs, const ip::IpAddress& lhs);
-		friend bool operator<(const ip::IpAddress& rhs, const ip::IpAddress& lhs);
-		friend std::ostream& operator<<(std::ostream& out, const ip::IpAddress& ip) {
-			out << ip.str();
-			return out;
-		}
-	};
-
+	friend bool operator==(const IpAddress& rhs, const IpAddress& lhs);
+	friend bool operator>(const IpAddress& rhs, const IpAddress& lhs);
+	friend bool operator<(const IpAddress& rhs, const IpAddress& lhs);
+	friend std::ostream& operator<<(std::ostream& out, const IpAddress& ip) {
+		out << ip.str();
+		return out;
+	}
 };
 
 
